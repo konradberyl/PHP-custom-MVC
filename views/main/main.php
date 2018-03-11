77b9b9b41a4a9c1248b845c72ecfa4a2b2fe096c
konrad <konrad.nekros@gmail.com>
2018-03-11 21:24:21 +0100

    add some css styles

--- views/main/main.php
+++ views/main/main.php
@@ -7,19 +7,32 @@
 
     </head>
     <body>
-        <div class="container">
+        <div class="container fullsize">
             <?php require 'views/main/menu.php'; ?>
+            <div class="content">
 
-            <div class="row">
                 <div class="text-center">
 
                     <h1>Welcome on the main page!</h1>
                     <h2>This webside is custom implementation of MVC</h2>
                     <h3>Based on PHP</h3>
                 </div>
+
+                <div class="flexbox-cards">
+                    <div class="first-card"><h1>M</h1></div>
+                    <div class="second-card"><h1>V</h1></div>
+                    <div class="third-card"><h1>C</h1></div>
+                </div>
+                <div class="home-img">
+                <img src="include/img/php.png">
+                </div>
+
+                <?php require 'views/main/basicJs.php'; ?>
             </div>
-            <?php require 'views/main/basicJs.php'; ?>
+            <?php require 'views/main/footer.php'; ?>
+        </div>
+
     </body>
-</div>
+
 
 </html>
--- views/user/list.php
+++ views/user/list.php
@@ -8,22 +8,25 @@
     </head>
 
     <body>
-        <div class="container">
+        <div class="container fullsize">
+                <?php require 'views/main/menu.php'; ?>
+            <div class="content">
+            
 
-            <?php require 'views/main/menu.php'; ?>
-
-            <div class="row">
-                <div class="col-sm-6 col-sm-offset-4">
-                    <h1>Available users: </h1>
-                    <ul>
-                        <?php foreach ($users as $user) { ?>
-                            <li><?php echo $user['name']; ?></li>
-                        <?php }
-                        ?>
-                    </ul>
+                <div class="row">
+                    <div class="col-sm-6 col-sm-offset-4">
+                        <h1>Available users: </h1>
+                        <ul>
+                            <?php foreach ($users as $user) { ?>
+                                <li><?php echo $user['name']; ?></li>
+                            <?php }
+                            ?>
+                        </ul>
+                    </div>
                 </div>
+                <?php require 'views/main/basicJs.php'; ?>
             </div>
-            <?php require 'views/main/basicJs.php'; ?>
+              <?php require 'views/main/footer.php'; ?>
     </body>
 </div>
 
